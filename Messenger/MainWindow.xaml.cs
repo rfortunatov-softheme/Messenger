d55de7ff6019f06e912bc2d40c4cbbf882f9ee8f
rfortunatov-softheme <roman.fortunatov@softheme.com>
2016-09-05 14:51:50 +0300

    Initial implementation. Needs a lot of work

---
+++ Messenger/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using CefSharp;
+
+namespace Messenger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var settings = new CefSettings();
+            settings.CefCommandLineArgs.Add("disable-gpu", "1");
+            Cef.Initialize(settings);
+            InitializeComponent();
+            Browser.Address = "https://messenger.com";
+        }
+    }
+}
